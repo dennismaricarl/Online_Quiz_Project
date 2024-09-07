bdc02aa7c7b7173f45c060f5c16a7729cd074916
dennismaricarl <dennismaricarl@gmail.com>
2024-09-06 19:39:03 -0700

    log in updated with showPassword useState

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,4 +1,16 @@
+import { FaRegEye } from "react-icons/fa6";
+import { FaRegEyeSlash } from "react-icons/fa6";
+import { useState } from "react";
+
+
+
 const Login = () => {
+
+    const [showPassword, setShowPassword] = useState(false)
+
+    const pass = () => {
+        setShowPassword(!showPassword)
+    }
 
     return (
         <div className="flex flex-col items-center pt-40 h-screen">
@@ -11,14 +23,35 @@ const Login = () => {
                         <input className="w-96 h-12 pl-5 focus:outline-none" type="email" />
                     </fieldset>
 
-                    <fieldset className="border p-1 rounded-md border-black bg-white mb-5">
+                    <fieldset className="border p-1 rounded-md border-black bg-white mb-5 relative">
                         <legend className="text-xl">Password*</legend>
-                        <input className="w-96 h-12 pl-5 focus:outline-none" type="password" />
+
+                        <input
+                            className="w-60 h-12 pl-5 focus:outline-none"
+                            type={showPassword ? "text" : "password"}
+                        />
+
+                        {/* top-1/2 => the element's top is positioned at the halfway point of the container -in this case <input/>.
+                transform -translate-y-1/2 => shifts the element upwards by 50% of its own height, 
+            making its center align with the center of the container. 
+                tranform allows for various transformations (translate, rotate, scale, etc)
+            */}
+
+                        {showPassword ? (
+                            <FaRegEye
+                                onClick={pass}
+                                className="size-5 absolute right-3 top-1/2 transform -translate-y-1/2"
+                            />
+                        ) : (
+                            <FaRegEyeSlash
+                                onClick={pass}
+                                className="size-5 absolute right-3 top-1/2 transform -translate-y-1/2"
+                            />
+                        )}
+
                     </fieldset>
 
-
-
-                    <h1 className="pt-10 text-xl">New to Online Quiz? <button className="bg-yellow-300 hover:bg-[#65c565] rounded-lg px-8" onClick={() => Navigate('/signUp')}>CREATE AN ACCOUNT</button></h1> 
+                    <h1 className="pt-10 text-xl">New to Online Quiz? <button className="bg-yellow-300 hover:bg-[#65c565] rounded-lg px-8" onClick={() => Navigate('/signUp')}>CREATE AN ACCOUNT</button></h1>
 
                     <button className="p-4 text-white text-xl rounded-md bg-[#185519] hover:bg-[#d6cf4f] mt-10">
                         Continue
